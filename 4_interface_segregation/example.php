8faac564aaf9f768072c3bb660019dbe15750fa6
najibu <ns.najibu@gmail.com>
2017-04-09 14:34:57 +0100

    Added interface segregation

---
+++ 4_interface_segregation/example.php
@@ -0,0 +1,53 @@
+<?php 
+// Interface Segregation 
+// States that a client should not be forced to implement an interface that it doesn't use.
+
+interface ManagableInterface {
+  public function beManaged();
+}
+
+interface WorkableInterface {
+  public function work();
+}
+
+interface SleepableInterface {
+  public function sleep();
+}
+
+class HumanWorker implements WorkableInterface, SleepableInterface, ManagableInterface {
+  public function work()
+  {
+
+  }
+
+  public function sleep()
+  {
+    return 'Human sleeping';
+  }
+
+  public function beManaged()
+  {
+    $this->work();
+    $this->sleep();
+  }
+}
+
+// Shouldnot force AndroidWorker to implement the sleep method
+class AndroidWorker implements WorkableInterface, ManagableInterface {
+  public function work()
+  {
+    return 'Android working';
+  }
+
+  public function beManaged()
+  {
+    $this->work();
+  }
+}
+
+class Captain {
+  public function manage(ManagableInterface $worker)
+  {
+    return $worker->beManaged();
+  }
+}
