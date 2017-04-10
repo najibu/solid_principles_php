2e439b28857650752d79b17d326844e32876a719
najibu <ns.najibu@gmail.com>
2017-04-10 11:12:01 +0100

    Added dependency inversion

---
+++ 5_dependency_inversion/example.php
@@ -0,0 +1,26 @@
+<?php
+// Dependency Inversion 
+// Depends on abstractions, not on concretions. 
+// This allows for decoupling code.
+
+interface ConnectionInterface {
+  public function connect();
+}
+
+// Low level code depends upon abstraction
+class DbConnection implements ConnectionInterface {
+  public function connect() 
+  {
+
+  }
+}
+
+// High level code depends upon abstraction
+class PasswordReminder {
+  protected $dbConnection;
+
+  public function __construct(ConnectionInterface $dbConnection)
+  {
+    $this->dbConnection = $dbConnection;
+  }
+}
